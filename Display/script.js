230cd8758842414624cbcbaac284a5627e21db56
Nicholas Jones <nicholasj898@gmail.com>
2023-08-04 20:11:12 -0700

    Added Button to Change Color

--- Display/script.js
+++ Display/script.js
@@ -3,3 +3,11 @@ websiteLink.addEventListener("click", function(event) {
   event.preventDefault();
   alert("Redirecting to webpage.");
 });
+
+const changeColorButton = document.getElementById("changeColorButton");
+changeColorButton.addEventListener("click", function() {
+  const informationDiv = document.querySelector(".information");
+  // Generate a random color in hexadecimal format
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  informationDiv.style.backgroundColor = randomColor;
+});
